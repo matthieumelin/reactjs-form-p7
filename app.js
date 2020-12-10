6e7481733d058e86af6c8aa4ae2a678d2739c7b3
matthieumelin <matthieumelin62@gmail.com>
2020-12-10 17:31:07 +0100

    the end

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 class App extends React.Component {
     state = {
-        comments: [ 
-            { name: "Your name", message: "Your comment" },
-        ]
+        name: "",
+        message: "",
+        comments: []
+    }
+
+    handleName = (event) => {
+        console.log("name: " + event.target.value);
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    handleMessage = (event) => {
+        console.log("message: " + event.target.value);
+        this.setState({
+            message: event.target.value
+        })
     }
 
     handleSubmit = (event) => {
         event.preventDefault();
 
+        const comment = {
+            name: this.state.name,
+            message: this.state.message
+        }
+        
+        const temp = this.state.comments.concat(comment);
+        
+        console.log(temp);
+
         this.setState({
-            comments: [
-                { name: event.target.value, message: event.target.value }
-            ]
+            comments: temp
         })
     }
 
@@ -19,8 +40,14 @@ class App extends React.Component {
         return (
             <form onSubmit={this.handleSubmit}>
                 <h3>Say something</h3>
-                <input type="text" name="name" id="name" value={this.state.comments[0].name} />
-                <textarea name="comment" id="comment" cols="30" rows="10" value={this.state.comments[0].message}></textarea>
+                <input type="text" name="name" id="name" 
+                placeholder="Your name"
+                value={this.state.name} 
+                onChange={this.handleName} />
+                <textarea name="comment" id="comment" cols="30" rows="10" 
+                placeholder="Your message"
+                value={this.state.message} 
+                onChange={this.handleMessage}></textarea>
                 <button type="submit">Comment</button>
             </form>
         )
